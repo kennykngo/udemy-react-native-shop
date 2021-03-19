285d7ad2c53e2b2b3068ee9dc748d9488c643ee7
Kenny Ngo <kenny.ngo.design@gmail.com>
2021-03-18 19:32:33 -0700

    optimized the CartScreen.js section with the /UI/Card.js

--- screens/shop/CartScreen.js
+++ screens/shop/CartScreen.js
@@ -85,13 +85,6 @@ const styles = StyleSheet.create({
     justifyContent: 'space-between',
     marginBottom: 20,
     padding: 10,
-    shadowColor: 'black',
-    shadowOpacity: 0.26,
-    shadowOffset: { width: 0, height: 2 },
-    shadowRadius: 8,
-    elevation: 5,
-    borderRadius: 10,
-    backgroundColor: 'white',
   },
   summaryText: {
     fontFamily: 'open-sans-bold',
