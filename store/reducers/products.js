9e6974f18ac25422b865006fea2c2ddbdc5f9bef
Kenny Ngo <kenny.ngo.design@gmail.com>
2021-03-18 17:06:56 -0700

    added the logic for UPDATE_PRODUCT

--- store/reducers/products.js
+++ store/reducers/products.js
@@ -1,6 +1,11 @@
 import PRODUCTS from '../../data/dummy-data';
+import Product from '../../models/product';
 
-import { DELETE_PRODUCT } from '../actions/products';
+import {
+  CREATE_PRODUCT,
+  DELETE_PRODUCT,
+  UPDATE_PRODUCT,
+} from '../actions/products';
 
 const initialState = {
   availableProducts: PRODUCTS,
@@ -9,6 +14,49 @@ const initialState = {
 
 export default (state = initialState, action) => {
   switch (action.type) {
+    case CREATE_PRODUCT:
+      const newProduct = new Product(
+        new Date().toString(),
+        'u1',
+        action.productData.title,
+        action.productData.imageUrl,
+        action.productData.description,
+        action.productData.price
+      );
+      // returns the initial state, then overrides the parts by combining the availbleProducts array with newProducts
+      return {
+        ...state,
+        availableProducts: state.availableProducts.concat(newProduct),
+        userProducts: state.userProducts.concat(newProduct),
+      };
+    case UPDATE_PRODUCT:
+      // remember that each product has an id and the pid is just the id
+      const productIndex = state.userProducts.findIndex(
+        (prod) => prod.id === action.pid
+      );
+      const updatedProduct = new Product(
+        action.pid,
+        state.userProducts[productIndex].ownerId,
+        action.productData.title,
+        action.productData.imageUrl,
+        action.productData.description,
+        state.userProducts[productIndex].price
+      );
+      // creates a new array and reassigns that array with the selected index and assigns it to the updatedProduct
+      const updatedUserProducts = [...state.userProducts];
+      updatedUserProducts[productIndex] = updatedProduct;
+      // same logic as the productIndex BUT searched the availableProducts array
+      const availableProductIndex = state.availableProducts.findIndex(
+        (prod) => prod.id === action.pid
+      );
+      const updatedAvailableProducts = [...state.availableProducts];
+      updatedAvailableProducts[availableProductIndex] = updatedProduct;
+      return {
+        ...state,
+        availableProducts: updatedAvailableProducts,
+        userProducts: updatedUserProducts,
+      };
+
     case DELETE_PRODUCT:
       // keep all products that is NOT the one that is selected
       return {
